616970294bce9bca31035a1dd08f6dd607a684db
999hyeon <dandan9509@gmail.com>
2020-02-09 16:01:06 +0900

    feat: storybook addon (#3)

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+
+addParameters({
+  backgrounds: [
+    { name: 'dark', value: '#232326', default: true },
+    { name: 'light', value: '#fff' },
+  ],
+  viewport: {
+    defaultViewport: 'someDefualt',
+  },
+})
 
 configure(
   require.context('../src/components', true, /\.stories\.tsx$/),
