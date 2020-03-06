97c6647efc7f482faa3ec0db25addcef70db9dac
Hansanghyeon <dandan9509@gmail.com>
2020-03-06 09:57:40 +0900

    feat(atom): menu btn

---
+++ src/components/atoms/Toggle/Menu/index.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import MenuBtn from '.';
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export default {
+  title: 'atoms/Toggle/Menu',
+  component: MenuBtn,
+};
+
+export const standard = () => (
+  <>
+    <GlobalStyle />
+    <MenuBtn />
+  </>
+);
