e96537b71e4451291149f2bdff562ab713313d36
Hansanghyeon <dandan9509@gmail.com>
2020-03-06 09:49:26 +0900

    chore: 필요없는 stories 삭제

--- src/components/atoms/FramerMotion/Button/MenuToggle/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MenuToggle from '.';
-
-export default {
-  title: 'atoms/Button/MenuToggle',
-  component: MenuToggle,
-};
-
-export const standard = () => <MenuToggle />;
