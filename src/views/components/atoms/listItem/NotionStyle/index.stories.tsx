ba4faafee7ca8bb65cdab2c8cee3fac15fbb507b
Hansanghyeon <dandan9509@gmail.com>
2020-03-15 19:15:08 +0900

    feat: notion list item knobs apply

--- src/views/components/atoms/listItem/NotionStyle/index.stories.tsx
+++ src/views/components/atoms/listItem/NotionStyle/index.stories.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withPreview } from 'storybook-addon-preview';
 // Context
 import CenterWrap from '@atom/wrap/Center';
 // Components
@@ -7,12 +9,17 @@ import NotionStyleList from '.';
 export default {
   title: '01. atoms/List Item/NotionStyle',
   component: NotionStyleList,
+  decorators: [
+    withKnobs,
+    withPreview,
+    (storyFn: any) => <CenterWrap>{storyFn()}</CenterWrap>,
+  ],
 };
-export const standard = () => (
-  <NotionStyleList imgSrc="https://wp.hapas.io/wp-content/uploads/React.png">
-    React
-  </NotionStyleList>
-);
-standard.story = {
-  decorators: [(storyFn: any) => <CenterWrap>{storyFn()}</CenterWrap>],
+export const standard = () => {
+  const imgSrc = text(
+    'Image src',
+    'https://wp.hapas.io/wp-content/uploads/icons/React.png',
+  );
+  const Text = text('Text', 'React');
+  return <NotionStyleList imgSrc={imgSrc}>{Text}</NotionStyleList>;
 };
