be1cd7235a6f7c226b2209c4d9dada0b90500ad7
Hansanghyeon <dandan9509@gmail.com>
2020-03-20 00:26:03 +0900

    style: storybook 커스텀 테마 및 스타일

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -6,6 +6,8 @@ import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 import '@storybook/addon-docs/register';
 import 'storybook-addon-preview/register';
+import "prism-themes/themes/prism-dracula.css";
+import './style.css';
 
 addons.setConfig({
   theme: themes.dark,
