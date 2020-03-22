3a1ad9258b1d6edd2582102f1bf28a76c14676e9
Hansanghyeon <dandan9509@gmail.com>
2020-03-22 17:40:09 +0900

    style: text 단계별 색상추가

--- src/views/styles/themes/4log.ts
+++ src/views/styles/themes/4log.ts
@@ -4,7 +4,7 @@ const DefaultTheme: StyleSystem = {
   color: {
     primary: '#0f4c81',
     bg: ['#f6f8ff'],
-    text: ['#333'],
+    text: ['#333', '#4A4F57', '#868E96'],
     grayscales: {
       dark: ['#12171f', '#161a23', '#181c27', '#1f2330', '#252836', '#303645'],
       light: ['#ffffff', '#e6ecf8', '#9ba4b7', '#6d7484', '#404655'],
