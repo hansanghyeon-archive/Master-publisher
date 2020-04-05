fdb840cb8b8998977305f675ead224577db92965
Hansanghyeon <dandan9509@gmail.com>
2020-04-05 22:48:01 +0900

    feat: 'SeoPreview Card' 컴포넌트 v1.0.0

---
+++ src/views/components/atoms/card/SeoPreview/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// import { withPreview } from 'storybook-addon-preview';
+// components
+import CenterWrap from '@atom/wrap/Center';
+import SeoPreviewCard from '.';
+
+export default {
+  title: '01. atoms/Card/SeoPreview',
+  component: SeoPreviewCard,
+  decorators: [(storyFn: any) => <CenterWrap>{storyFn()}</CenterWrap>],
+};
+
+export const standard = () => {
+  const props = {
+    data: {
+      site_name: 'Medium',
+      type: 'article',
+      title: 'Puppeteer 간단 정리하기',
+      description:
+        'Puppeteer 는 Headless Chrome 혹은 Chromium 를 제어하도록 도와주는 라이브러리 이다.',
+      url:
+        'https://medium.com/@pks2974/puppeteer-%EA%B0%84%EB%8B%A8-%EC%A0%95%EB%A6%AC%ED%95%98%EA%B8%B0-a252bffbb2a8',
+      image:
+        'https://fs.nas.hapas.io/meta-crawler/https%253A%252F%252Fmedium.com%252F@pks2974%252Fpuppeteer-%25EA%25B0%2584%25EB%258B%25A8-%25EC%25A0%2595%25EB%25A6%25AC%25ED%2595%2598%25EA%25B8%25B0-a252bffbb2a8/image.png',
+      favicon:
+        'https://fs.nas.hapas.io/meta-crawler/https%253A%252F%252Fmedium.com%252F@pks2974%252Fpuppeteer-%25EA%25B0%2584%25EB%258B%25A8-%25EC%25A0%2595%25EB%25A6%25AC%25ED%2595%2598%25EA%25B8%25B0-a252bffbb2a8/favicon.ico',
+    },
+  };
+  return <SeoPreviewCard {...props} />;
+};
