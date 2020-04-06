4f0321d4e91ddbf5688bdc3e172b738858f7bf9b
999hyeon <dandan9509@gmail.com>
2020-04-06 22:02:09 +0900

    feat: SEO Preview UI v1.0.0 (#28)
    * feat: sass설정 셋팅
    
    * feat: 'SeoPreview Card' 컴포넌트 v1.0.0
    
    * test: api 테스트용 커스텀훅 제작
    
    * chore(pakage): 'Lottie' 패키지 & json import ts 옵션설정
    
    * feat: loading animation component
    
    * feat: 로딩 크롬 non-favicon 아이콘 svg
    
    * feat: 로딩일때 상태추가
    
    * fix: req data를 아예 못받을때 undefined되서 error나는 부분수정

---
+++ src/views/components/atoms/card/SeoPreview/index.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Row, Col } from 'styled-bootstrap-grid';
+import './index.style.scss';
+import LoadingIcon from './LoadingIcon';
+import LoadingLottie from './LoadingLottie';
+
+const Header = styled.div`
+  background-color: ${({ theme }) => theme.color.bg[0]};
+`;
+const Favicon = styled.div``;
+const Body = styled(Row)`
+  background-color: ${({ theme }) => theme.color.bg[1]};
+  color: ${({ theme }) => theme.color.text[2]};
+`;
+const Url = styled.div``;
+const ImageCol = styled(Col)<styleProps>`
+  background-image: ${({ bg }) => `url(${bg})`};
+`;
+const Content = styled(Col)``;
+const RootWrap = styled.a`
+  ${Row} {
+    margin-left: 0;
+    margin-right: 0;
+  }
+  &:hover {
+    ${Header} {
+      background-color: ${({ theme }) => theme.color.primary};
+      color: ${({ theme }) => theme.color.grayscales.light[0]};
+    }
+    ${Body} {
+      color: ${({ theme }) => theme.color.text[1]};
+      border: 1px solid ${({ theme }) => theme.color.text[2]};
+    }
+  }
+`;
+const LottieWrap = styled(Col)`
+  svg.cpu-chip path[stroke='rgb(0,0,0)'] {
+    stroke: ${({ theme }) => theme.color.text[1]};
+  }
+  svg.cpu-chip path[stroke='rgb(241,241,241)'] {
+    stroke: ${({ theme }) => theme.color.bg[1]};
+  }
+  svg.cpu-chip path[fill='rgb(0,0,0)'] {
+    fill: ${({ theme }) => theme.color.text[1]};
+  }
+  svg.cpu-chip path[fill='rgb(241,241,241)'] {
+    fill: ${({ theme }) => theme.color.bg[1]};
+  }
+`;
+const Loaded = ({ data }: reqData) => {
+  const { title, description, url, favicon, image } = data;
+  const decodeUrl = decodeURI(url);
+  return (
+    <>
+      <Header className="_header">
+        <Favicon className="_favicon">
+          <img src={favicon} alt="" />
+        </Favicon>
+        <div>{title}</div>
+      </Header>
+      <Body className="_body">
+        <Content col className="_content">
+          <div className="_description">{description}</div>
+          <Url className="_url">{decodeUrl}</Url>
+        </Content>
+        {image && <ImageCol col={12} sm={4} bg={image} className="_thumnail" />}
+      </Body>
+    </>
+  );
+};
+const Loading = () => (
+  <>
+    <Header className="_header">
+      <Favicon className="_favicon">
+        <LoadingIcon />
+      </Favicon>
+      <div>Loading...</div>
+    </Header>
+    <Body className="_body">
+      <LottieWrap col className="_content">
+        <LoadingLottie />
+      </LottieWrap>
+    </Body>
+  </>
+);
+
+const SeoPreviewCard = ({ data, loading, reqUrl }: props) => {
+  return (
+    <RootWrap href={reqUrl} className={`seoPreview ${loading && 'loading'}`}>
+      {!loading ? <Loaded data={data} /> : <Loading />}
+    </RootWrap>
+  );
+};
+
+export default SeoPreviewCard;
+interface styleProps {
+  bg?: string;
+}
+interface reqData {
+  data?: {
+    title: string;
+    description: string;
+    image: string;
+    favicon: string;
+    url: string;
+  };
+}
+interface props extends reqData {
+  loading: boolean;
+  reqUrl: string;
+}
