356ed622dfc02b1ab114bf31e4a5d35b7289f943
Hansanghyeon <dandan9509@gmail.com>
2020-04-26 20:26:50 +0900

    feat: react-flow-chart 연습

--- src/views/components/pages/notion/#001/index.stories.tsx
+++ src/views/components/pages/notion/#001/index.stories.tsx
@@ -4,6 +4,7 @@ import { withKnobs } from '@storybook/addon-knobs';
 // components
 import CenterWrap from '@atom/wrap/Center';
 import COMPONENT from '.';
+import ReactFlowChart from './ReactFlowChart';
 
 export default {
   title: 'notion 🗃/SPA란?',
@@ -38,4 +39,111 @@ export const standard2 = () => {
 };
 standard2.story = {
   name: 'SPA Lifecycle',
+};
+
+export const standard3 = () => {
+  const data = {
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    nodes: {
+      nodeLayout: {
+        id: 'nodeLayout',
+        type: 'layout',
+        position: {
+          x: 8,
+          y: 8,
+        },
+        ports: {
+          port1: {},
+        },
+      },
+      node1: {
+        id: 'node1',
+        type: 'output-only',
+        position: {
+          x: 300,
+          y: 100,
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'output',
+            properties: {
+              value: 'yes',
+            },
+          },
+          port2: {
+            id: 'port2',
+            type: 'output',
+            properties: {
+              value: 'no',
+            },
+          },
+        },
+      },
+      node2: {
+        id: 'node2',
+        type: 'input-output',
+        position: {
+          x: 300,
+          y: 300,
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'input',
+          },
+          port2: {
+            id: 'port2',
+            type: 'output',
+          },
+        },
+      },
+      nodeTitle: {
+        id: 'nodeTitle',
+        type: 'what-is-spa',
+        position: {
+          x: 0,
+          y: 24,
+        },
+        properties: {
+          title: 'Tranditional Page Lifecycle',
+        },
+        ports: {
+          port1: {
+            id: 'port1',
+            type: 'right',
+            properties: {
+              custom: 'property',
+            },
+          },
+          port2: {
+            id: 'port2',
+            type: 'right',
+            properties: {
+              custom: 'property',
+            },
+          },
+        },
+      },
+    },
+    links: {
+      link1: {
+        id: 'link1',
+        from: {
+          nodeId: 'node1',
+          portId: 'port2',
+        },
+        to: {
+          nodeId: 'node2',
+          portId: 'port1',
+        },
+      },
+    },
+    selected: {},
+    hovered: {},
+  };
+  return <ReactFlowChart data={data} />;
 };
