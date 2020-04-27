61b827491085eae1a6fc8631596067c8d5915a3a
Hansanghyeon <dandan9509@gmail.com>
2020-04-27 15:24:40 +0900

    chore(training): d3 'Creating a Pie Chart'

--- src/views/components/training/D3/#001/index.tsx
+++ src/views/components/training/D3/#001/index.tsx
@@ -13,6 +13,7 @@ const D3_001 = () => {
   const Svg = useRef<SVGSVGElement>(null);
   const Svg2 = useRef<SVGSVGElement>(null);
   const Svg3 = useRef<SVGSVGElement>(null);
+  const Svg4 = useRef<SVGSVGElement>(null);
   useEffect(() => {
     // Train: Creating a Simple Bar Chart
     (() => {
@@ -144,6 +145,41 @@ const D3_001 = () => {
         .attr('r', 80)
         .attr('fill', '#9ce8d5');
     })();
+
+    // Train: Creating a Pie Chart
+    (() => {
+      const MockDataset = [
+        { platform: 'Android', percentage: 40.11 },
+        { platform: 'Windows', percentage: 36.69 },
+        { platform: 'IOS', percentage: 13.06 },
+      ];
+
+      const svgWidth = 500;
+      const svgHeight = 300;
+      const radius = Math.min(svgWidth, svgHeight) / 2;
+
+      const svg = d3
+        .select(Svg4.current)
+        .attr('width', svgWidth)
+        .attr('height', svgHeight);
+
+      const g = svg
+        .append('g')
+        .attr('transform', `translate(${radius}, ${radius})`);
+
+      const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+      const pie = d3.pie().value((d) => d.percentage);
+
+      const path = d3.arc().outerRadius(radius).innerRadius(0);
+
+      const arc = g.selectAll('arc').data(pie(MockDataset)).enter().append('g');
+
+      arc
+        .append('path')
+        .attr('d', path)
+        .attr('fill', (d) => color(d.data.percentage));
+    })();
   }, []);
 
   return (
@@ -156,6 +192,8 @@ const D3_001 = () => {
       <svg ref={Svg2} />
       <br />
       <svg ref={Svg3} />
+      <br />
+      <svg ref={Svg4} />
     </>
   );
 };
