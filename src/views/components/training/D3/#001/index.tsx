73c3b5c255aef01981e5feb5cf344fc3cc01ecf9
Hansanghyeon <dandan9509@gmail.com>
2020-04-27 14:54:33 +0900

    chore(training): d3 'Axes'

--- src/views/components/training/D3/#001/index.tsx
+++ src/views/components/training/D3/#001/index.tsx
@@ -4,6 +4,7 @@ import * as d3 from 'd3';
 const D3_001 = () => {
   const Root = useRef<HTMLDivElement>(null);
   const Svg = useRef<SVGSVGElement>(null);
+  const Svg2 = useRef<SVGSVGElement>(null);
   useEffect(() => {
     // Train: Creating a Simple Bar Chart
     (() => {
@@ -58,16 +59,53 @@ const D3_001 = () => {
         .data(MockDataset)
         .enter()
         .append('text')
-        .text((d) => d)
-        .attr('y', (d) => svgHeight - d - 2)
-        .attr('x', (d, i) => barWidth * i)
+        .text((d) => yScale(d))
+        .attr('y', (d) => svgHeight - yScale(d) + 20)
+        .attr('x', (d, i) => barWidth * i + 4)
         .attr('fill', '#A64C38');
+    })();
+
+    // Train: Axes
+    (() => {
+      const MockDataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+      const svgWidth = 500;
+      const svgHeight = 300;
+
+      const svg = d3
+        .select(Svg2.current)
+        .attr('width', svgWidth)
+        .attr('height', svgHeight);
+
+      const xScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(MockDataset)])
+        .range([0, svgHeight]);
+
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(MockDataset)])
+        .range([0, svgHeight]);
+
+      const xAxis = d3.axisBottom().scale(xScale);
+      const yAxis = d3.axisLeft().scale(yScale);
+
+      svg.append('g').attr('transform', 'translate(50, 10)').call(yAxis);
+
+      const xAxisTranslate = svgHeight - 20;
+
+      svg
+        .append('g')
+        .attr('transform', `translate(50, ${xAxisTranslate})`)
+        .call(xAxis);
     })();
   }, []);
   return (
     <>
       <div ref={Root} />
+      <br />
       <svg ref={Svg} />
+      <br />
+      <svg ref={Svg2} />
     </>
   );
 };
