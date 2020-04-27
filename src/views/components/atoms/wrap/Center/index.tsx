ee28dbdf201ba8da0272ca2019b0d37b5763bfc4
Hansanghyeon <dandan9509@gmail.com>
2020-04-27 14:54:15 +0900

    feat: overflow component 처리

--- src/views/components/atoms/wrap/Center/index.tsx
+++ src/views/components/atoms/wrap/Center/index.tsx
@@ -5,8 +5,9 @@ const Center = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
+  min-height: 100%;
   width: 100%;
+  padding: 20px;
 `;
 
 export default Center;
