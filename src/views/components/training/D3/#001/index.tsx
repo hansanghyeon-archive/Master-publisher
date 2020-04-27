690c23a525ddbfff2b6871472a15c3b59dafc775
Hansanghyeon <dandan9509@gmail.com>
2020-04-27 14:11:38 +0900

    chore(training): d3 'Data Loading and Binding'

--- src/views/components/training/D3/#001/index.tsx
+++ src/views/components/training/D3/#001/index.tsx
@@ -3,23 +3,17 @@ import * as d3 from 'd3';
 
 const D3_001 = () => {
   const Root = useRef<HTMLDivElement>(null);
-  const Heaing = useRef<HTMLDivElement>(null);
+  const MockDataset = [1, 2, 3, 4, 5];
   useEffect(() => {
-    d3.select(Heaing.current)
-      .style('color', 'red')
-      .attr('class', 'heading')
-      .text('Updated h1 tag');
-    d3.select(Root.current).append('p').text('First Paragraph');
-    d3.select(Root.current).append('p').text('Second Paragraph');
-    d3.select(Root.current).append('p').text('Third Paragraph');
-
-    d3.selectAll('p').style('color', 'blue');
+    d3.select(Root.current)
+      .selectAll('p')
+      .data(MockDataset)
+      .enter()
+      .append('p')
+      // .text('D3 is awesome!!');
+      .text((d) => d);
   }, []);
-  return (
-    <div ref={Root}>
-      <h1 ref={Heaing}>Frist D3</h1>
-    </div>
-  );
+  return <div ref={Root} />;
 };
 
 export default D3_001;
