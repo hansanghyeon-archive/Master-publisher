4b80e2d3acce308f8a431ff9e5abd1f6ad5673b5
Hansanghyeon <dandan9509@gmail.com>
2020-05-02 18:43:21 +0900

    chore(remove): 필요없는 사용하지않는 컴포넌트 제거

--- src/views/components/atoms/FramerMotion/Button/Refresh/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Refresh from '.';
-
-export default {
-  title: '01. atoms/Button/Refresh',
-  component: Refresh,
-};
-
-export const standard = () => <Refresh />;
--- src/views/components/training/FramerMotion/KeyFrames/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import KeyFrame from '.';
-
-export default {
-  title: '99. training/Framer motion/KeyFrames',
-  component: KeyFrame,
-};
-
-export const standard = () => <KeyFrame />;
