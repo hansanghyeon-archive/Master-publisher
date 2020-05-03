ecf1137662c1ccda0138fc5f74e00f85e31daa40
999hyeon <dandan9509@gmail.com>
2020-05-03 21:45:43 +0900

    feat: bit.dev도입 컴포넌트 버저닝 🎉 (#38)
    * chore(setting): github 폴더 visible
    
    * chore(remove): 필요없는 사용하지않는 컴포넌트 제거
    
    * feat: ci/cd vercel 이전 누락 코드추가 & dev 패키지 이전
    
    * fix: division node 'setTime' window 'setTime'
    
    * feat: bit.dev config

--- src/views/components/training/FramerMotion/Animation/Scale/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Scale from '.';
-
-export default {
-  title: '99. training/Framer motion/Animation/Scale',
-  component: Scale,
-};
-
-export const standard = () => <Scale />;
--- src/views/components/training/FramerMotion/Variants/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Variants from '.';
-
-export default {
-  title: '99. training/Framer motion/Variants',
-  component: Variants,
-};
-
-export const standard = () => <Variants />;
