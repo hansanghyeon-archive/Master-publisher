8347ca32196aed1a016c68998aa510d4d3fcb75d
Hansanghyeon <dandan9509@gmail.com>
2020-05-04 23:56:07 +0900

    chore(remove): isDark 사용제거

--- src/views/styles/themes/index.ts
+++ src/views/styles/themes/index.ts
@@ -3,12 +3,10 @@ import * as _4log from '@style/themes/4log';
 const themes = [
   {
     name: '[4log] Default',
-    isDark: false,
     color: _4log.default.color,
   },
   {
     name: '[4log] Dark',
-    isDark: true,
     color: _4log.DarkTheme.color,
   },
 ];
