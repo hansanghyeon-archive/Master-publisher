898757f1f518cfc5a1b62fd9075ec7c5f4826ee8
Hansanghyeon <dandan9509@gmail.com>
2020-08-28 18:41:45 +0900

    chore(package): npm update or storybook composition

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   refs: {
     hapas_todos: {
-      title: 'TODOs',
-      url: 'https://hapas-io.github.io/TODOs/',
+      title: 'Hyupup',
+      url: 'https://hyupup-storybook.vercel.app/',
     },
   },
 };
