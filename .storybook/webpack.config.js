cafdbecd0981af2fd81fe0df8cbe98e3a3100703
999hyeon <999@hyeon.pro>
2021-09-28 11:38:24 +0900

    :construction: wip

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -17,19 +17,19 @@ module.exports = ({ config }) => {
       presets: [['react-app', { flow: false, typescript: true }]],
     },
   });
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [
-      'style-loader',
-      'css-loader',
-      {
-        loader: 'sass-loader',
-        options: {
-          prependData: `@import '~@style/global';`,
-        },
-      },
-    ],
-  });
+  // config.module.rules.push({
+  //   test: /\.scss$/,
+  //   use: [
+  //     'style-loader',
+  //     'css-loader',
+  //     {
+  //       loader: 'sass-loader',
+  //       options: {
+  //         prependData: `@import '~@style/global';`,
+  //       },
+  //     },
+  //   ],
+  // });
   config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx');
   config.resolve.alias = {
     '@': path.resolve(__dirname, '../src/components'),
